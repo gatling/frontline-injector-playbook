70e6bdaeeac8ce9405233b25613b3173d8606898
Guillaume Corré <gcorre@gatling.io>
2021-10-04 12:56:52 +0200

    Migrate marketplace release job to GitHub Actions and automate Azure SAS URLs generation, close OPS-78, close OPS-71

--- packer/build-probe-aws.sh
+++ packer/build-probe-aws.sh
@@ -2,21 +2,13 @@
 
 set -eu
 
-profile="$1"
-build_region="$2"
-java_major="$3"
+build_region="$1"
+java_major="$2"
+java_version="$3"
 
-copy_regions=$(aws --profile "$profile" ec2 describe-regions --query "Regions[?RegionName != '$build_region'].RegionName" --output text | tr -s '[:blank:]' ',')
-
-adoptopenjdk_url="https://api.adoptium.net/v3/assets/latest/${java_major}/hotspot"
-java_version=$(curl -Ls "${adoptopenjdk_url}" | jq -r '.[] | select(
-    (.binary.os == "linux") and
-    (.binary.architecture == "x64") and
-    (.binary.image_type == "jdk")
-  ) | .version.openjdk_version')
+copy_regions=$(aws ec2 describe-regions --query "Regions[?RegionName != '$build_region'].RegionName" --output text | tr -s '[:blank:]' ',')
 
 packer build \
-  -var "profile=$profile" \
   -var "region=$build_region" \
   -var "copy_regions=$copy_regions" \
   -var "java_major=$java_major" \
--- packer/build-probe-azure.sh
+++ packer/build-probe-azure.sh
@@ -2,42 +2,26 @@
 
 set -eu
 
-client_id="$1"
-client_secret="$2"
-java_major="$3"
-location="$4"
-resource_group="$5"
-ssh_private_key_file="$6"
-ssh_username="$7"
-storage_account="$8"
-subscription_id="$9"
+java_major="$1"
+java_version="$2"
 
-adoptopenjdk_url="https://api.adoptopenjdk.net/v2/latestAssets/releases/openjdk${java_major}?openjdk_impl=hotspot&os=linux&arch=x64&release=latest&type=jdk"
-java_version=$(curl -Ls "${adoptopenjdk_url}" | jq -r 'map(.version_data.openjdk_version) | .[]')
+location=westeurope
+resource_group=GatlingFrontLineInjectorsRG
+ssh_username=azure-user
+storage_account=injectorsst
 
 # Make sure we end up with a valid VHD name
 java_version=${java_version//+/_}
 
-# Check if client_id, client_secret and subscription_id where given as files or strings
-if [[ -f "$client_id" ]]; then
-  client_id=$(cat "$client_id")
-fi
-if [[ -f "$client_secret" ]]; then
-  client_secret=$(cat "$client_secret")
-fi
-if [[ -f "$subscription_id" ]]; then
-  subscription_id=$(cat "$subscription_id")
-fi
-
 packer build \
-  -var "client_id=$client_id" \
-  -var "client_secret=$client_secret" \
+  -var "client_id=$CLIENT_ID" \
+  -var "client_secret=$CLIENT_SECRET" \
   -var "java_major=$java_major" \
   -var "java_version=$java_version" \
   -var "location=$location" \
   -var "resource_group=$resource_group" \
-  -var "ssh_private_key_file=$ssh_private_key_file" \
+  -var "ssh_private_key_file=$SSH_PRIVATE_KEY_FILE" \
   -var "ssh_username=$ssh_username" \
   -var "storage_account=$storage_account" \
-  -var "subscription_id=$subscription_id" \
+  -var "subscription_id=$SUBSCRIPTION_ID" \
   template-probe-azure.json
