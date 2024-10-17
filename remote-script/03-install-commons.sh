88627a0bb9d171fdc4b995307e96d5b6219fb3cd
Davy MELINA <davy.melina@gmail.com>
2024-10-17 18:22:03 +0200

    Add libxcrypt-compat for older compat

--- remote-script/03-install-commons.sh
+++ remote-script/03-install-commons.sh
@@ -6,5 +6,6 @@ echo "-- installing commons                                                 --"
 echo "------------------------------------------------------------------------"
 
 sudo su << EOT
-yum install -y jq
+dnf install -y jq
+dnf install -y libxcrypt-compat
 EOT
