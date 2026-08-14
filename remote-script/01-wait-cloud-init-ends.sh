70899e7df3112c486bfe64f4084c8ce4c321bb69
eric molle <eric.molle.new@gmail.com>
2026-08-14 11:34:29 +0200

    fixed: shell script format

--- remote-script/01-wait-cloud-init-ends.sh
+++ remote-script/01-wait-cloud-init-ends.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 
-
 echo "------------------------------------------------------------------------"
 echo "-- wait cloud init ends                                               --"
 echo "------------------------------------------------------------------------"
 
-sudo su << EOT
+sudo su <<EOT
 while ! grep "Cloud-init .* finished" /var/log/cloud-init.log; do
     echo "$(date -Ins) Waiting for cloud-init to finish"
     sleep 2
